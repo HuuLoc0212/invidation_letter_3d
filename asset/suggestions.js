5fc69ac110e06e1a936370072a0005993d9ccb0d
HuuLoc0212 <nguynlc885@gmail.com>
2023-10-19 00:57:40 +0700

    test 1

---
+++ asset/suggestions.js
@@ -0,0 +1,13 @@
+let suggestions = [
+    "Nguyễn Đức Thắng",
+    "Nguyễn Thị Diễm Quỳnh",
+    "Văn Bảo Nguyên",
+    "Phạm Ngọc Trân",
+    "Hoa Kiều",
+    "Lê Phương Thảo",
+    "Trần Thiện Thanh",
+    "Bùi Trương Mạnh Thông",
+    "Lạc Tiến Huy",
+    "Võ Anh Kiệt",
+];
+export default suggestions;
